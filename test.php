4f58b965d83e2bd9f968f1c8afef48f18b7ea031
Maleesha Gimshan <47087449+maleeshagimshan98@users.noreply.github.com>
2021-06-07 01:18:05 +0530

    updated test.php file

--- test.php
+++ test.php
@@ -2,7 +2,6 @@
 
 include_once __DIR__."/pdo_connection.php";
 include_once __DIR__."/vendor/autoload.php";
-include_once __DIR__."/Products.php";
 
 use Infinite\DataMapper\EntityManager;
 use Infinite\DataMapper\Entity\Product;
@@ -17,14 +16,16 @@ $entityManager = new EntityManager ((object) [
 
 /** GET PRODUCTS */
 
-//$product = $entityManager->get("product",["id","llmp_001"])->go('llmp_001');
+//$product = $entityManager->get("product",["id"=>"llmp_001"])->go('llmp_001');
+$product = $entityManager->get("product")->associate("product_sku",["sku_id" => "c_80ml_va"])->go(["c_80ml_va"]);
 
 //$product = $entityManager->get("product")->go();
 
 //$product = $entityManager->get("product")->associate("product_sku")->go();
-//$product1 = $product[0];
-//$product1->product_name = "llmp_test_0002";
-//$entityManager->save($product1)->go();
+/*
+$product1 = $product[0]; 
+$product1->product_name = "llmp_test_0002";
+$entityManager->save($product1)->go();*/
 //$entityManager->save($product1);
 echo json_encode($product);
 
@@ -40,4 +41,9 @@ $entityManager->go();*/
 
 /**----------------------- */
 
+/** Get login record */
+//$loginRecord = $entityManager->get("login_record",["user_id" =>"1"])->go('1');
+//echo json_encode($loginRecord);
+
+/** */
 ?>
